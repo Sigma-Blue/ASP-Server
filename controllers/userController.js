d3763c25d0ae645efb1b9a23a294467079f334b0
Metta Surendhar <msurendhar8815@gmail.com>
2023-10-23 16:19:50 +0530

    seperated the isUserExist middlware to userMiddleware in Middleware folder

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,5 @@
 const userModel = require('./../models/userModel');
 
-// Middlewares :
-
-//	@desc		Check whether the user exists => if exists then call next()
-//	@body		password
-
-exports.isUserExist = async (req, res, next) => {
-	const userName = req.params.userName;
-
-	const { result: selectedUser, error: selectedErr } =
-		await userModel.selectUserInfoByUserName(userName);
-
-	if (selectedErr) {
-		return res.status(500).json({
-			status: 'Failure: SelectedErr ',
-			message: `Internal Server Error : ${selectedErr}`,
-		});
-	}
-
-	if (!selectedUser) {
-		return res.status(404).json({
-			status: 'Failure: SelectedUser',
-			message: `User: ${userName} does not exist`,
-		});
-	}
-
-	req.body.id = selectedUser.id;
-
-	next();
-};
-
-// Controllers :
-
 //	@route	POST	/register
 //	@desc		Register New User
 //	@body		userName,email,password
---
+++ middlewares/userMiddleware.js
@@ -0,0 +1,30 @@
+const userModel = require('./../models/userModel');
+
+//	@route	PATCH	/resetPassword/:userName
+//	@desc		Check whether the user exists => if exists then call next()
+//	@body		password
+
+exports.isUserExist = async (req, res, next) => {
+	const userName = req.params.userName;
+
+	const { result: selectedUser, error: selectedErr } =
+		await userModel.selectUserInfoByUserName(userName);
+
+	if (selectedErr) {
+		return res.status(500).json({
+			status: 'Failure: SelectedErr ',
+			message: `Internal Server Error : ${selectedErr}`,
+		});
+	}
+
+	if (!selectedUser) {
+		return res.status(404).json({
+			status: 'Failure: SelectedUser',
+			message: `User: ${userName} does not exist`,
+		});
+	}
+
+	req.body.id = selectedUser.id;
+
+	next();
+};
