1f6da0a797b2a9541185d64544db290be1775626
RojiniAthira <rojiniathirap@gmail.com>
2023-11-20 12:28:03 +0530

    created model for homepage

--- routes/v1/homedirectRouter.js
+++ routes/v1/homedirectRouter.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const postController = require("../../controllers/postController");
-const postMiddleware = require("../../middlewares/postMiddleware");
+const postController = require("./../../controllers/postController");
 
 router.route("/:userId/").get(postController.getHomePage);
 
